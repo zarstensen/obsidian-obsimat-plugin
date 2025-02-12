950693b32c37c1f3ac1f288deee16ecb2aa37bbd
karstensensensen <simonblsoerensen@gmail.com>
2025-02-12 20:18:37 +0100

    tmp commit

--- src/SympyEvaluator.ts
+++ src/SympyEvaluator.ts
@@ -62,6 +62,8 @@ export class SympyEvaluator {
         });
     }
 
+// TODO: remove this
+
     // Start the SympyEvaluator python process, and establish an connection to it.
     // vault_dir: the directory of the vault, which thsi plugin is installed in.
     // python_exec: the python executable to use to start the SympyEvaluator process.

5fbea1b5950065c21de10824758af24d7ec252ce
karstensensensen <simonblsoerensen@gmail.com>
2025-02-19 18:37:23 +0100

    Fix tests

--- src/evaluator/tests/Evaluate_test.py
+++ src/evaluator/tests/Evaluate_test.py
@@ -18,7 +18,7 @@ class TestEvaluate:
         result = response.getResult()
             
         with evaluate(False):
-            assert result['result'] == Eq(2, 2)
+            assert result['result'] == 2
             
     def test_escaped_spaces(self):
         response = TestResponse()
@@ -28,7 +28,7 @@ class TestEvaluate:
         result = response.getResult()
             
         with evaluate(False):
-            assert result['result'] == Eq(2, 2)
+            assert result['result'] == 2
         
         
     def test_matrix_single_line(self):
@@ -74,7 +74,7 @@ class TestEvaluate:
         
         result = response.getResult()
 
-        assert result['result'].rhs == sqrt(20**2 + 30**2 + 40**2 + 50**2)
+        assert result['result'] == sqrt(20**2 + 30**2 + 40**2 + 50**2)
         
     def test_matrix_inner_prodcut(self):
         response = TestResponse()
@@ -96,7 +96,7 @@ class TestEvaluate:
         
         result = response.getResult()
 
-        assert result['result'].rhs == 1 * 2 + 2 * 4
+        assert result['result'] == 1 * 2 + 2 * 4
         
         
     def test_relational_evaluation(self):
@@ -111,7 +111,7 @@ class TestEvaluate:
         
         result = response.getResult()
 
-        assert result['result'].rhs == 20
+        assert result['result'] == 20
         
         response.reset()
         asyncio.run(evaluateMode({"expression": r"""
@@ -133,7 +133,7 @@ class TestEvaluate:
         
         result = response.getResult()
 
-        assert result['result'].rhs == 1
+        assert result['result'] == 1
         
         response.reset()
         asyncio.run(evaluateMode({"expression": r"""
@@ -147,7 +147,7 @@ class TestEvaluate:
         
         result = response.getResult()
         
-        assert result['result'].rhs == 8
+        assert result['result'] == 8
         assert result['metadata']['start_line'] == 4
         assert result['metadata']['end_line'] == 4
         

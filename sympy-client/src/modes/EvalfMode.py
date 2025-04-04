17564161cc5a15b7fe4bdea53408c2069a39a3b8
Philip Rying <philiprying@gmail.com>
2025-04-04 19:20:20 +0200

    add recursive evalfmode

--- sympy-client/src/modes/EvalfMode.py
+++ sympy-client/src/modes/EvalfMode.py
@@ -9,11 +9,18 @@ from sympy import *
 async def evalf_handler(message: EvaluateMessage, response: ModeResponse, parser: SympyParser):
     def evaluate(sympy_expr):
 
-        # If it is a dictonary, we cant call doit or simplify on it
-        if not isinstance(sympy_expr, dict):
+        if isinstance(sympy_expr, dict):
+            res = {}
+            for key, value in sympy_expr.items():
+                new_key = evaluate(key)
+                new_value = evaluate(value)
+                res[new_key] = new_value
+            sympy_expr = res
+        elif isinstance(sympy_expr, (int, float)):
+            return sympy_expr
+        else:
             sympy_expr = try_assign(sympy_expr.evalf(), sympy_expr)
-        # TODO: Recursive call when it is returned
-        
+
         return sympy_expr
            
     return await eval_mode_base(message, response, parser, evaluate)

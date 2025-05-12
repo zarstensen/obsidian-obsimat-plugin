eee52ab19fa9f2f84eb2dac761752a0d283f1f63
Simon Sørensen <simonblsoerensen@gmail.com>
2025-05-12 22:02:34 +0200

    update version check

--- version-check.py
+++ version-check.py
@@ -8,4 +8,10 @@ MANIFEST_FILE = "manifest.json"
 with open(MANIFEST_FILE, 'r') as file:
     manifest = json.load(file)
 
-sys.exit(0 if expected_version == manifest['version'] else -1)
+if expected_version == manifest['version']:
+    print("Versions Match!")
+else:
+    print("Versions Do Not Match!")
+    print(f"Manifest Version:\t{manifest['version']}")
+    print(f"Expected Version:\t{expected_version}")
+    sys.exit(-1)
